4c29ab63397ec55925d4eee5b4512a792e46579c
Rishit Gupta <74411873+Rishit30G@users.noreply.github.com>
2025-02-05 10:31:09 +0530

    Update global CSS styles and font weights

--- app/page.tsx
+++ app/page.tsx
@@ -1,88 +1,94 @@
 "use client";
 
 import { Input } from "@/components/ui/input";
-import { CornerDownRight, Shuffle, Youtube } from "lucide-react";
+import { CornerDownRight, Shuffle } from "lucide-react";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 import Image from "next/image";
 import SkeletonCard from "@/components/SkeletonCard";
 import { useEffect, useState } from "react";
-import { Switch } from "@/components/ui/switch";
-
-
-
-const placeholders = [
-  "I purchased spinach today, what can I make out of it ?",
-  "What's a good recipe for a healthy breakfast ?",
-  "How can I make a healthy smoothie ?",
-  "What are some healthy snacks I can make ?",
-  "Any healthy recipes with broccoli?"
-]; 
-const useDynamicPlaceholder = (placeholders: string[], interval: number) => {
-  const [placeholder, setPlaceholder] = useState("");
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const speed = 30; // Typing speed per character
-
-  useEffect(() => {
-    let charIndex = 0;
-    let typingInterval: NodeJS.Timeout;
-
-    const typeText = () => {
-      if (charIndex <= placeholders[currentIndex].length) {
-        setPlaceholder(placeholders[currentIndex].slice(0, charIndex));
-        charIndex++;
-      } else {
-        clearInterval(typingInterval);
-        setTimeout(() => {
-          setCurrentIndex((prev) => (prev + 1) % placeholders.length);
-        }, interval - placeholders[currentIndex].length * speed); // Adjust delay before switching
-      }
-    };
-
-    typingInterval = setInterval(typeText, speed);
-
-    return () => clearInterval(typingInterval);
-  }, [currentIndex, interval, placeholders]);
-
-  return placeholder;
-};
+import useFetch from "@/hooks/use-fetch";
+import { getDetails, getImage } from "@/actions/dashboard";
+import {
+  placeholders,
+  useDynamicPlaceholder,
+} from "@/hooks/useDynamicPlaceholder";
+import Link from "next/link";
 
 export default function Home() {
-
-  const [searchQuery, setSearchQuery] = useState("How can I make Vegetable Stir-fry");
-  const [videoUrl, setVideoUrl] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchYouTubeVideo = async() => {
-      if(!searchQuery) return;
-      try{
-        const response = await fetch(`api/youtube?query=${searchQuery}`);
-        const data = await response.json(); 
-        if(data.videoUrl){
-          console.log(data.videoUrl);
-        }
-      }catch(error){
-        console.error("Failed to fetch video", error);
-      }
-    };
-    fetchYouTubeVideo();
-  }, [searchQuery]);
-
+  const [userInput, setUserInput] = useState("");
+  const [cardData, setCardData] = useState<any[]>([]);
+  const [imageUrl, setImageUrl] = useState("");
 
   const placeholder = useDynamicPlaceholder(placeholders, 6000);
+  const {
+    loading: geminiLoading,
+    data: geminiData,
+    fn: geminiFn,
+  } = useFetch(getDetails);
+
+  const {
+    loading: imageLoading,
+    data: imageData,
+    fn: imageFn,
+  } = useFetch(getImage);
+
+  const handleSubmit = async () => {
+    if (!userInput.trim()) return;
+    await geminiFn(userInput);
+  };
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      await imageFn("Healthy food with broccoli, spinach, and avocado");
+      
+    };
+    fetchImage();
+  }, []);
+
+  useEffect(() => {
+    if(imageData){
+      setImageUrl(imageData);
+    }
+  }
+  , [imageData]);
+
+  useEffect(() => {
+    const fetchYoutubeVideo = async () => {
+      if(geminiData && geminiData.length > 0){
+        const updatedData = await Promise.all(
+          geminiData.map(async (item: any) => {
+            try{
+              const response = await fetch(`api/youtube?query=${item.title}`);
+              const data = await response.json();
+              return {
+                ...item, 
+                youtube: data.videoUrl
+              }
+            }catch(error){
+              console.error("Failed to fetch video", error);
+              return {
+                ...item,
+                youtube: null
+              };
+            }
+          })
+        ); 
+        setCardData(updatedData);
+      }
+    };
+    fetchYoutubeVideo();
+  }, [geminiData]);
 
   return (
     <div className="container max-w-3xl mx-auto">
       {/* Header */}
       <div className="flex justify-center flex-col items-center gap-2 mt-5">
         <h1 className="text-7xl outfit-extrabold tracking-wide bg-gradient-to-br from-green-300 via-green-600 to-green-500 text-transparent bg-clip-text animate-gradient-bg max-md:text-5xl">
-         Healthy F
+          Healthy F
           <img
             className="inline-block object-contain size-12 animate-bounce-slow max-md:size-16"
             src="https://i.postimg.cc/SsbJKSKr/pngtree-healthy-food-png-png-image-10154104.png"
@@ -90,76 +96,91 @@ export default function Home() {
           />
           od
         </h1>
-        <p className="text-md text-green-700 outfit-regular-italic">Discover the best healthy dishes curated for you</p> 
+        <p className="text-md text-green-700 outfit-regular-italic">
+          Discover the best healthy dishes curated for you
+        </p>
       </div>
 
       {/* Search  */}
       <div className="mt-10 flex-col flex items-center ">
         <div className="relative w-[600px] max-md:w-[400px] ">
-          <Input placeholder={placeholder} className="rounded-full bg-green-800/15 w-full caret-green-800 shadow-green-800/20 shadow-lg"/>
-          <Shuffle size={26} className="text-green-700 absolute right-12 top-1/2 transform -translate-y-1/2 bg-green-100 rounded-full p-1 cursor-pointer"/>
-          <CornerDownRight size={26} className="text-green-700 absolute right-3 top-1/2 transform -translate-y-1/2 bg-green-100 rounded-full p-1 cursor-pointer"/>
+          <Input
+            placeholder={placeholder}
+            className="rounded-full bg-green-800/15 w-full caret-green-800 shadow-green-800/20 shadow-lg"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+          />
+          <Shuffle
+            size={26}
+            className="text-green-700 absolute right-12 top-1/2 transform -translate-y-1/2 bg-green-100 rounded-full p-1 cursor-pointer"
+          />
+          <CornerDownRight
+            size={26}
+            className="text-green-700 absolute right-3 top-1/2 transform -translate-y-1/2 bg-green-100 rounded-full p-1 cursor-pointer"
+            onClick={handleSubmit}
+          />
         </div>
         <div className="flex justify-end mr-2 w-[600px] max-md:w-[400px]">
-          <h3 className="text-green-600 outfit-regular text-xs mt-2">Powered by <img className="inline-block object-contain size-6" src="https://i.postimg.cc/v8zR1Bfd/Gemini-Icon-300x300-png.webp"/></h3>
+          <h3 className="text-green-600 outfit-regular text-xs mt-2">
+            Powered by{" "}
+            <img
+              className="inline-block object-contain size-6"
+              src="https://i.postimg.cc/v8zR1Bfd/Gemini-Icon-300x300-png.webp"
+            />
+          </h3>
         </div>
       </div>
 
-
       {/* Cards  */}
       <div className="grid grid-cols-2 my-10 place-items-center gap-8 max-lg:grid-cols-1 max-lg:px-5">
-        <SkeletonCard/>
-        <Card className="bg-green-100/30 shadow-lg rounded-t-xl">
-            <CardContent className="space-y-5">
-              <Image src="/image.jpg" alt="Dish" width={500} height={100} className="rounded-tr-lg rounded-tl-lg"/> 
-              <div className="flex justify-between flex-col w-full gap-2 px-3">
-                <h2 className="text-2xl text-green-700 outfit-bold">Healthy Salad</h2>
-                <h5 className="text-green-800 text-sm tracking-tight outfit-regular">This creamy broccoli soup is a comforting and nutritious meal. It's packed with vitamins, minerals, and fiber, making it a healthy choice for any diet. Enjoy this delicious and wholesome soup on a chilly evening.</h5>
-              </div>
-            </CardContent>
-            <CardFooter className="justify-end items-center px-4 mb-3">
-                <Image src="https://www.freeiconspng.com/thumbs/youtube-logo-png/hd-youtube-logo-png-transparent-background-20.png" alt="YouTube" className="inline-block object-contain cursor-pointer" width={30} height={30} />
-            </CardFooter>
-        </Card>
-
-        <Card className="bg-green-100/30 shadow-lg rounded-t-xl">
-            <CardContent className="space-y-5">
-              <Image src="/image.jpg" alt="Dish" width={500} height={100} className="rounded-tr-lg rounded-tl-lg"/> 
-              <div className="flex justify-between flex-col w-full gap-2 px-3">
-                <h2 className="text-2xl text-green-700 outfit-bold">Watermelon Juice</h2>
-                <h5 className="text-green-800 text-sm tracking-tight outfit-regular">A vibrant, crunchy salad packed with vitamins and fiber, perfect for a light lunch or side dish. 🍉</h5>
-              </div>
-            </CardContent>
-            <CardFooter className="justify-end items-center px-4 mb-3">
-                <Image src="https://www.freeiconspng.com/thumbs/youtube-logo-png/hd-youtube-logo-png-transparent-background-20.png" alt="YouTube" className="inline-block object-contain cursor-pointer" width={30} height={30} />
-            </CardFooter>
-        </Card>
-
-        <Card className="bg-green-100/30 shadow-lg rounded-t-xl">
-            <CardContent className="space-y-5">
-              <Image src="/image.jpg" alt="Dish" width={500} height={100} className="rounded-tr-lg rounded-tl-lg"/> 
-              <div className="flex justify-between flex-col w-full gap-2 px-3">
-                <h2 className="text-2xl text-green-700 outfit-bold">Healthy Salad</h2>
-                <h5 className="text-green-800 text-sm tracking-tight outfit-regular">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid iure necessitatibus obcaecati, eum reiciendis. Id doloribus esse harum atque.</h5>
-              </div>
-            </CardContent>
-            <CardFooter className="justify-end items-center px-4 mb-3">
-                <Image src="https://www.freeiconspng.com/thumbs/youtube-logo-png/hd-youtube-logo-png-transparent-background-20.png" alt="YouTube" className="inline-block object-contain cursor-pointer" width={30} height={30} />
-            </CardFooter>
-        </Card>
-
-        <Card className="bg-green-100/30 shadow-lg rounded-t-xl">
-            <CardContent className="space-y-5">
-              <Image src="/image.jpg" alt="Dish" width={500} height={100} className="rounded-tr-lg rounded-tl-lg"/> 
-              <div className="flex justify-between flex-col w-full gap-2 px-3">
-                <h2 className="text-2xl text-green-700 outfit-bold">Healthy Salad</h2>
-                <h5 className="text-green-800 text-sm tracking-tight outfit-regular">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid iure necessitatibus obcaecati, eum reiciendis. Id doloribus esse harum atque.</h5>
-              </div>
-            </CardContent>
-            <CardFooter className="justify-end items-center px-4 mb-3">
-                <Image src="https://www.freeiconspng.com/thumbs/youtube-logo-png/hd-youtube-logo-png-transparent-background-20.png" alt="YouTube" className="inline-block object-contain cursor-pointer" width={30} height={30} />
-            </CardFooter>
-        </Card>
+        {/* {geminiLoading ? (
+          <SkeletonCard />
+        ) : (
+          cardData?.map((card, index) => (
+            <Card
+              key={index}
+              className="bg-green-100/30 shadow-lg rounded-t-xl"
+            >
+              <CardContent className="space-y-5">
+                <Image
+                  src="/image.jpg"
+                  alt="Dish"
+                  width={500}
+                  height={100}
+                  className="rounded-tr-lg rounded-tl-lg"
+                />
+                <div className="flex justify-between flex-col w-full gap-2 px-3">
+                  <h2 className="text-2xl text-green-700 outfit-bold">
+                    {card.title}
+                  </h2>
+                  <h5 className="text-green-800 text-sm tracking-tight outfit-regular w-[100%]">
+                    {card.description}
+                  </h5>
+                </div>
+              </CardContent>
+              <CardFooter className="justify-end items-center px-5 mb-3">
+              {card.youtube && (
+                <Link href={card.youtube} target="_blank" rel="noopener noreferrer">
+                  <Image
+                    src="https://www.freeiconspng.com/thumbs/youtube-logo-png/hd-youtube-logo-png-transparent-background-20.png"
+                    alt="YouTube"
+                    className="inline-block object-contain cursor-pointer"
+                    width={30}
+                    height={30}
+                  />
+                </Link>
+              )}
+              </CardFooter>
+            </Card>
+          ))
+        )} */}
+        {
+          imageLoading ? (
+            <SkeletonCard />
+          ) : (
+            <Image src={imageUrl} alt="Dish" width={500} height={100} />
+          )
+        }
       </div>
     </div>
   );

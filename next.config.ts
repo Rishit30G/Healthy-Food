88199a7cf7eadd719472fb69dc2996376658b1ba
Rishit Gupta <74411873+Rishit30G@users.noreply.github.com>
2025-02-09 16:34:13 +0530

    Add new domain to images configuration

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["images.pexels.com", "www.freeiconspng.com", "www.freeiconspng.com"],
-  }
+    domains: ["images.pexels.com", "www.freeiconspng.com", "www.freeiconspng.com", "i.postimg.cc"],
+  },
 };
 
 export default nextConfig;
